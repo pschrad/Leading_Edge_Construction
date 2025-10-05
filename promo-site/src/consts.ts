41616517fe5bf1a11e3cb88e7b7ac8e0b75b08ad
Phyona Schrader <pschrad2@nd.edu>
2025-10-05 10:26:01 +0000

    new styling for homepage

--- promo-site/src/consts.ts
+++ promo-site/src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// Global site data
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Leading Edge Construction';
+export const SITE_DESCRIPTION = 'Luxury pools and outdoor living environments crafted for resort-style everyday living in Central Florida.';
